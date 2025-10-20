817dd1ed1beb84ef0882786b32e2817c9fa75b54
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2025-10-20 16:31:39 +0200

    Creació de fitxers i menú principal

---
+++ PE04_AcarretaAdrian.java
@@ -0,0 +1,40 @@
+package Activitats.PE04;
+
+import java.util.Scanner;
+
+public class PE04_AcarretaAdrian {
+    static Boolean H1;
+    static Boolean H2;
+    static Boolean H3;
+    static Boolean bathroom;
+    static Boolean kitchen;
+    static Boolean menjador;
+    static String r;
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Lights control:\n");
+        System.out.println("  a. Control a room");
+        System.out.println("  b. Control all the rooms");
+        System.out.println("  c. Show the real state of the lights");
+        System.out.println("  d. Exit");
+        r = s.next();
+        switch (r) {
+            case "a":
+                
+                break;
+            case "b":
+                
+                break;
+            case "c":
+                
+                break;
+            case "d":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
